1f80744653d05ccf3683b6b63a874cadaf2628db
hydrapheetz <hydrapheetz@gmail.com>
2011-11-11 21:47:09 -0600

    added hacks.php (layout hacks)

---
+++ lib/hacks.php
@@ -0,0 +1,10 @@
+<?php
+	// layout hacks made easy (and clean)
+	// TODO: make this load from external files
+	$hacks = Array();
+	
+	$hacks["layout"] = array(
+		"ikachan" => function($image = "/images/squid.png") {
+			return "<img src='$image' style='position: fixed; left: ".mt_rand(0,100)."%;
+				top: ".mt_rand(0,100)."%'>";
+		});
