8d659c09589e33ffa1d02164e7c28c18c6ecbc4c
Xkeeper <xkeeper@gmail.com>
2011-11-13 17:18:51 -0800

    Making mysql trigger_error on query failure

--- lib/mysql.php
+++ lib/mysql.php
@@ -1,57 +1,55 @@
 <?php
-  class mysql{
-    var $queries=0;
-    var $rowsf=0;
-    var $rowst=0;
-    var $time=0;
-    function connect($host,$user,$pass) {return mysql_connect($host,$user,$pass);}
-    function selectdb($dbname)          {return mysql_select_db($dbname);}
+	class mysql{
+		var $queries=0;
+		var $rowsf=0;
+		var $rowst=0;
+		var $time=0;
+		function connect($host,$user,$pass) {return mysql_connect($host,$user,$pass);}
+		function selectdb($dbname)          {return mysql_select_db($dbname);}
 
-    function query($query){
-//      if($_GET[q])
-//        print "$query<br>";
-      $start=microtime(true);
-      if($res=mysql_query($query)){
-        $this->queries++;
-        $this->rowst+=@mysql_num_rows($res);
-      } else {
-			// print mysql_error();
-	  }
+		function query($query){
+			$start=microtime(true);
+			if($res=mysql_query($query)){
+				$this->queries++;
+				$this->rowst+=@mysql_num_rows($res);
+			} else {
+				trigger_error("MySQL error: ". mysql_error(), E_USER_WARNING);
+			}
 
-      $this->time+=microtime(true)-$start;
-      return $res;
-    }
+			$this->time+=microtime(true)-$start;
+			return $res;
+		}
 
-    function fetch($result){
-      $start=microtime(true);
+		function fetch($result){
+			$start=microtime(true);
 
-      if($result && $res=mysql_fetch_array($result, MYSQL_ASSOC))
-          $this->rowsf++;
+			if($result && $res=mysql_fetch_array($result, MYSQL_ASSOC))
+				$this->rowsf++;
 
-      $this->time+=microtime(true)-$start;
-      return $res;
-    }
+			$this->time+=microtime(true)-$start;
+			return $res;
+		}
 
-    function result($result,$row=0,$col=0){
-      $start=microtime(true);
+		function result($result,$row=0,$col=0){
+			$start=microtime(true);
 
-      if($result && $res=@mysql_result($result,$row,$col))
-        $this->rowsf++;
+			if($result && $res=@mysql_result($result,$row,$col))
+			  $this->rowsf++;
 
-      $this->time+=microtime(true)-$start;
-      return $res;
-    }
+			$this->time+=microtime(true)-$start;
+			return $res;
+		}
 
-    function fetchq($query,$row=0,$col=0){
-      $res=$this->query($query);
-      $res=$this->fetch($res);
-      return $res;
-    }
+		function fetchq($query,$row=0,$col=0){
+			$res=$this->query($query);
+			$res=$this->fetch($res);
+			return $res;
+		}
 
-    function resultq($query,$row=0,$col=0){
-      $res=$this->query($query);
-      $res=$this->result($res,$row,$col);
-      return $res;
-    }
-  }
+		function resultq($query,$row=0,$col=0){
+			$res=$this->query($query);
+			$res=$this->result($res,$row,$col);
+			return $res;
+		}
+	}
 ?>
