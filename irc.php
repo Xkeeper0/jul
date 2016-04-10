e644949c0704228185e3496804cb90124436e029
RanAwaySuccessfully <RanAwaySuccessfully@users.noreply.github.com>
2016-04-10 18:08:56 -0300

    Embedding KiwiIRC
    Hopefully nothing breaks.

--- irc.php
+++ irc.php
@@ -10,7 +10,7 @@ require 'lib/layout.php';
 
 
 print "		$header<br>$tblstart<tr>
-		$tccellh><b>Java IRC Chat - BadnikNET, #x</b></td></tr>
+		$tccellh><b>IRC Chat - BadnikNET, #tcrf</b></td></tr>
 		<tr>$tccell1>Server List: ";
 
 foreach ($servers as $num => $name) {
@@ -38,38 +38,7 @@ print "		<tr>$tccell2>";
 	
 print "
 	
-	<applet code=\"IRCApplet.class\" codebase=\"irc/\"  
-	archive=\"irc.jar,pixx.jar\" width=\"100%\" height=500>
-	<param name=\"CABINETS\" value=\"irc.cab,securedirc.cab,pixx.cab\">
-	
-	<param name=\"nick\" value=\"". $name ."\">
-	<param name=\"alternatenick\" value=\"". $name ."_??\">
-	<param name=\"fullname\" value=\"Jul Java IRC User\">
-	<param name=\"host\" value=\"". $servers[$server] ."\">
-	<param name=\"gui\" value=\"pixx\">
-	<param name=\"authorizedcommandlist\" value=\"all-server-s\">
-	<param name=\"authorizedleavelist\" value=\"all-#x\">
-	<param name=\"authorizedjoinlist\" value=\"all\">
-
-	<param name=\"quitmessage\" value=\"JulIRC - http://jul.rustedlogic.net/\">
-	<param name=\"autorejoin\" value=\"true\">
-	
-	<param name=\"style:bitmapsmileys\" value=\"false\">
-	<param name=\"style:backgroundimage\" value=\"false\">
-	<param name=\"style:backgroundimage1\" value=\"none+Channel all 2 background.png.gif\">
-	<param name=\"style:sourcecolorrule1\" value=\"all all 0=000000 1=ffffff 2=0000ff 3=00b000 4=ff4040 5=c00000 6=c000a0 7=ff8000 8=ffff00 9=70ff70 10=00a0a0 11=80ffff 12=a0a0ff 13=ff60d0 14=a0a0a0 15=d0d0d0\">
-	
-	<param name=\"pixx:timestamp\" value=\"true\">
-	<param name=\"pixx:highlight\" value=\"true\">
-	<param name=\"pixx:highlightnick\" value=\"true\">
-	<param name=\"pixx:nickfield\" value=\"false\">
-	<param name=\"pixx:styleselector\" value=\"true\">
-	<param name=\"pixx:setfontonstyle\" value=\"true\">
-
-	". ($_GET['channel'] != "retro" ? "<param name=\"command1\" value=\"/join #x\"><param name=\"command1\" value=\"/join #tcrf\">" : "") ."
-	". ($_GET['channel'] ? "<param name=\"command1\" value=\"/join #". $_GET['channel'] ."\">" : "") ."
-	
-	</applet>";
+	<iframe src=\"https://kiwiirc.com/client/". $servers[$server] ."/?nick=". $name ."|?#tcrf\" style=\"border:0;width:100%;height:500px;\"></iframe>";
 
 	} else {
 
