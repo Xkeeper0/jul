38735e91a55b27112468831675bde892d0e29ca6
Kak2X <kak32x@gmail.com>
2016-12-23 13:15:22 +0100

    Add thread descriptions
    hopefully stuff shouldn't break

--- forum.php
+++ forum.php
@@ -280,6 +280,10 @@
 
 		$threadtitle	= "<a href='thread.php?id=$thread[id]'>$thread[title]</a>";
 		$belowtitle   = array(); // An extra line below the title in certain circumstances
+		
+		// The thread description has its own line though
+		if ($threaddesc = trim($thread['description']))
+			$threadtitle .= "<br><font class='fonts'>".str_replace(array('<', '>'), array('&lt;', '&gt;'), $threaddesc)."</font>";
 
 		$sicon			= "";
 		if ($thread['sticky'])	{
